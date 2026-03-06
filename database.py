a8cd34d4ddbcf156ce93814bad09b0e0fe9f5ab2
qakukushkinozon <mwemaill4@gmail.com>
2026-03-06 15:27:22 +0300

    Добавлен .env и .env.example для хранения переменных БД. Добавлена первая версия файла с взаимодействием с БД database.py

---
+++ database.py
@@ -0,0 +1,28 @@
+import mysql.connector as mysql
+import os
+from dotenv import load_dotenv
+
+db = None
+
+load_dotenv()
+
+try:
+    db = mysql.connect(
+        user=os.getenv("USER"),
+        passwd=os.getenv("PASSWD"),
+        host=os.getenv("HOST"),
+        port=int(os.getenv("PORT")),
+        database=os.getenv("DATABASE")
+    )
+
+    with db:
+        with db.cursor() as cursor:
+            cursor.execute(
+                '''
+                SELECT * FROM students
+                '''
+            )
+            print(cursor.fetchall())
+
+except (mysql.Error, TypeError, ValueError) as e:
+    print(f'Error: {e}')
