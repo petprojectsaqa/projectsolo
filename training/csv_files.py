6138526cdd0ec6ed9371b46a921b60d4e28c9593
Aleksey K <aqa.github@proton.me>
2026-03-12 16:12:17 +0300

    поработал с defaultdict и csv

---
+++ training/csv_files.py
@@ -0,0 +1,7 @@
+import csv
+
+
+with open('data.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
--- training/default_dict.py
+++ training/default_dict.py
@@ -1,2 +1,11 @@
-def zaglushka():
-    pass
+import collections
+
+with open('tasks_qa.txt') as tasks_file:
+    tasks = list(map(lambda x: x.replace('\n', ''), tasks_file.readlines()))
+
+
+task_dict = collections.defaultdict(list)
+for line in tasks:
+    tester_name, task = line.split(':')
+    task_dict[tester_name].append(task)
+print(task_dict)
