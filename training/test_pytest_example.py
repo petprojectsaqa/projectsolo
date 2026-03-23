e1a3b430cf7b29c3d05e59aaf60538bc98be953c
qakukushkinozon <mwemaill4@gmail.com>
2026-03-23 13:39:58 +0300

    добавлен conftest, перенесены фикстуры

--- training/test_pytest_example.py
+++ training/test_pytest_example.py
@@ -2,23 +2,6 @@ import httpx
 import pytest
 
 
-@pytest.fixture()
-def new_post_id():
-    body = {"title": "title_name", "body": "body_name", "userId": 1}
-    headers = {'Content-Type': 'application/json'}
-    response = httpx.post('https://jsonplaceholder.typicode.com/posts', json=body, headers=headers)
-    post_id = response.json()['id']
-    post_id = 99  # песочница не создает реальную сущность, поэтому подменяем её
-    yield post_id
-    print(f'Deleting the post, id: {post_id}')
-    httpx.delete(f'https://jsonplaceholder.typicode.com/posts/{post_id}')
-
-@pytest.fixture(scope='session')
-def hello():
-    print('hello')
-    yield
-    print('end')
-
 @pytest.mark.regression
 def test_get_one_post(new_post_id):
     response = httpx.get(f'https://jsonplaceholder.typicode.com/posts/{new_post_id}').json()
